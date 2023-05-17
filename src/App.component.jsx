dcbcfa8e97e849461aaf341b52ffc697250bb601
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-05-17 12:02:13 -0300

    Initial setup.

---
+++ src/App.component.jsx
@@ -0,0 +1,7 @@
+import { AppStyled } from "./App.styles";
+
+function App() {
+    return <AppStyled></AppStyled>;
+}
+
+export default App;
---
+++ src/App.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const AppStyled = styled.div`
+    background-color: var(--secondary-color-3);
+    min-width: 100vw;
+    min-height: 100vh;
+`;
